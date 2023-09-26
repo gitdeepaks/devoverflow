998a2baa892405a85dd664217f6921cef98e89c3
gitdeepaks <deepaksankhyan92@outlook.com>
2023-09-26 20:47:06 +0530

    new commit🚀

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="h1-bold flex-center">Hello World</h1>
-      <h2 className="h2-bold flex-center">Hello World</h2>
-      <h3 className="h3-bold flex-center">Hello World</h3>
+      <h1 className="h1-bold flex-center">Hello World form Next Js 13</h1>
+      <h2 className="h2-bold flex-center">Hello World form Next Js 13</h2>
+      <h3 className="h3-bold flex-center">Hello World form Next Js 13</h3>
     </div>
   );
 }
