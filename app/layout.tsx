e094102a44755f8f113ac5ceaddba33262dde9c6
gitdeepaks <deepaksankhyan92@outlook.com>
2023-10-02 20:06:05 +0530

    new commit🚀

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Space_Grotesk } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Create Next App",
+  title: "StackDevOverFlow Clone",
   description: "StackOverFlow Clone",
+  icons: {},
 };
 
 export default function RootLayout({
