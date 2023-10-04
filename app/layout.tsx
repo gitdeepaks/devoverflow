8029912e19358577682cc41a82445127ea306805
gitdeepaks <deepaksankhyan92@outlook.com>
2023-10-04 17:08:43 +0530

    new commit🚀

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,23 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrowtesk",
+});
 
 export const metadata: Metadata = {
-  title: "StackDevOverFlow Clone",
-  description: "StackOverFlow Clone",
-  icons: {},
+  title: "StackDevOverFlow",
+  description: "StackOverFlow but for developers",
+  icons: {
+    icon: "./assets/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -19,7 +30,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">This is the page</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
